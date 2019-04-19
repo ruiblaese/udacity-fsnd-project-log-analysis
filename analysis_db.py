5014ce0384fd45c6f0a93d9e30ac36b5b429d378
Rui Richard Blaese <ruiblaese@gmail.com>
2019-04-19 09:27:21 -0300

    fixed problems and implemented suggestions

--- analysis_db.py
+++ analysis_db.py
@@ -3,71 +3,86 @@
 import datetime
 import psycopg2
 
-#dev
-#config_db = "dbname=news user=postgres password=pass host=localhost"
+# dev
+config_db = "dbname=news user=postgres password=pass host=localhost"
 
-#vagrant
-config_db = "dbname=news"
+# vagrant
+# config_db = "dbname=news"
+
+
+def database_connect(database_name):
+    """Connect to the database.  Returns a database connection."""
+    try:
+        db = psycopg2.connect(dbname=database_name)
+        return db
+
+    except psycopg2.Error as e:
+        # THEN you could print an error
+        # and perhaps exit the program
+        print("Unable to connect to database")
+        sys.exit(1)
+
 
 def get_top_articles():
-  
-  db = psycopg2.connect(config_db)
-  c = db.cursor()
-  query = """\
-          select 
+    db = psycopg2.connect(config_db)
+    c = db.cursor()
+    query = """\
+          select
             articles.title,
-            count(log.id)		
+            count(log.id) as views
           from log
-          inner join articles on position(articles.slug in log.path) > 0
-          where
-            log.status = '200 OK'
-          group by 
-            articles.title
-          order by 
-            count(log.id) desc
+          inner join articles on concat('/article/', articles.slug) = log.path
+          where log.status = '200 OK'
+          group by articles.title
+          order by count(log.id) desc
           limit 3
-          """  
-  c.execute(query)
-  rows = c.fetchall()
-  db.close()  
-  return rows
+          """
+    c.execute(query)
+    rows = c.fetchall()
+    db.close()
+    return rows
+
 
 def get_top_authors():
-  db = psycopg2.connect(config_db)
-  c = db.cursor()
-  query = """\
-          select 
+    db = psycopg2.connect(config_db)
+    c = db.cursor()
+    query = """\
+          select
             authors.name,
-            count(log.id)		
+            count(log.id) as views
           from log
-          inner join articles on position(articles.slug in log.path) > 0
+          inner join articles on concat('/article/', articles.slug) = log.path
           inner join authors on authors.id = articles.author
-          where
-            log.status = '200 OK'
-          group by 
-            authors.name
-          order by 
-            count(log.id) desc
-          """  
-  c.execute(query)
-  rows = c.fetchall()
-  db.close()  
-  return rows
+          where (log.status = '200 OK')
+          group by authors.name
+          order by count(log.id) desc
+          """
+    c.execute(query)
+    rows = c.fetchall()
+    db.close()
+    return rows
+
 
 def get_top_errors():
-  db = psycopg2.connect(config_db)
-  c = db.cursor()
-  query = """\
+    db = psycopg2.connect(config_db)
+    c = db.cursor()
+    query = """\
           select
-            cast("time" as date),
-            cast( (( count(id)filter (where status <> '200 OK') * 100.00) / cast(count(id)as numeric)) as numeric(18,4)) as perc_error
+              cast("time" as date),
+              cast(
+                  (( count(id)filter (where status <> '200 OK') * 100.00) /
+                  cast(count(id)as numeric)) as numeric(18,4)
+              ) as perc_error
           from log
-          group by 
-            cast("time" as date)
+          group by
+              cast("time" as date)
           having
-            cast( (( count(id)filter (where status <> '200 OK') * 100.00) / cast(count(id)as numeric)) as numeric(18,4)) >= 1.00
-          """  
-  c.execute(query)
-  rows = c.fetchall()
-  db.close()  
-  return rows  
+              cast(
+                  ((count(id)filter (where status <> '200 OK') * 100.00) /
+                  cast(count(id)as numeric)) as numeric(18,4)
+              ) >= 1.00
+          """
+    c.execute(query)
+    rows = c.fetchall()
+    db.close()
+    return rows
--- analysis_server.py
+++ analysis_server.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# 
+#
 # A buggy web service in need of a database.
 
 from flask import Flask, request, redirect, url_for
@@ -15,7 +15,7 @@ HTML_WRAP = '''\
   <head>
     <title>Project: Log Analysis</title>
     <style>
-      h2{ text-align: center; }      
+      h2{ text-align: center; }
       div.post { border: 1px solid #999;
                  padding: 10px 10px;
                  margin: 10px 20%%;}
@@ -24,15 +24,15 @@ HTML_WRAP = '''\
     </style>
   </head>
   <body>
-    <h2>Top 3 Articles</h2>    
+    <h2>Top 3 Articles</h2>
     <!-- post content will go here -->
 %s
     <br>
-    <h2>Top Authors</h2>    
+    <h2>Top Authors</h2>
 %s
     <br>
-    <h2>Day did more than 1%% errors</h2>    
-%s    
+    <h2>Day did more than 1%% errors</h2>
+%s
   </body>
 </html>
 '''
@@ -46,17 +46,26 @@ TOP_ERRORS_DIV = '''\
     <div class=post>%s     - <em> %s%% errors</em></div>
 '''
 
+
 @app.route('/', methods=['GET'])
 def main():
-  '''Main page of the forum.'''
+    '''Main page of the forum.'''
 
-  top_articles_html = "".join(TOP_ARTICLES_AUTHORS_DIV % (title, views) for title, views in get_top_articles())
-  top_authors_html  = "".join(TOP_ARTICLES_AUTHORS_DIV % (name, views) for name, views in get_top_authors())
-  top_errors_html  = "".join(TOP_ERRORS_DIV % (date, errors) for date, errors in get_top_errors())
-  html = HTML_WRAP % (top_articles_html, top_authors_html, top_errors_html)
-  return html
+    top_articles_html = "".join(TOP_ARTICLES_AUTHORS_DIV %
+                                (title, views) for title, views in
+                                get_top_articles())
+
+    top_authors_html = "".join(TOP_ARTICLES_AUTHORS_DIV %
+                               (name, views) for name, views in
+                               get_top_authors())
+
+    top_errors_html = "".join(TOP_ERRORS_DIV %
+                              (date, errors) for date, errors in
+                              get_top_errors())
+
+    html = HTML_WRAP % (top_articles_html, top_authors_html, top_errors_html)
+    return html
 
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=8000)
-
+    app.run(host='0.0.0.0', port=8000)
